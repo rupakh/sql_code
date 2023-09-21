c26f60fb7f3ad2949669166d5d0ced15da7e9d82
rupakh <h.rupak@gmail.com>
2023-09-20 20:42:08 -0400

    Update Regions_count_descending.sql

--- Canada_Immigration_Queries/Regions_count_descending.sql
+++ Canada_Immigration_Queries/Regions_count_descending.sql
@@ -1,4 +1,4 @@
--- Query to find the regions' count to descending order
+-- Query to find the regions' count to descending order for Github 
 
 select region,count(*) from can_imm.canada_data
 group by region
