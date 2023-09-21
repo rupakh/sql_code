7f581a680a8d75aa43fa44f175c6c21455f68889
rupakh <h.rupak@gmail.com>
2023-09-20 21:12:44 -0400

    Update Regions_count_descending.sql

--- Canada_Immigration_Queries/Regions_count_descending.sql
+++ Canada_Immigration_Queries/Regions_count_descending.sql
@@ -4,6 +4,8 @@ select region,count(*) from can_imm.canada_data
 group by region
 order by count(*) desc;
 
+-- This query shows that Western Asia is the region from where most immigrants are coming to Canada (country wise)
+
 /*
 Western Asia	18
 Eastern Africa	17
